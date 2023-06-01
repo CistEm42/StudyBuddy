a816bfe077d2abfbdf42dd09b641cd1fff42f006
CistEm42 <durosimiterry53@gmail.com>
2023-06-02 00:00:16 +0100

    changes for deployment

--- base/urls.py
+++ base/urls.py
@@ -14,3 +14,4 @@ urlpatterns = [
      path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
 
 ]
+urlpatterns = urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
